9840b7f1329be765a68f85e16980ea92f626858f
qkdflrgs <qkdflrgs@gmail.com>
2024-07-17 18:56:59 +0900

    feat(banner): add EventBannerAddButton for batch event banners

---
+++ src/components/test/EventBannerAddButton.tsx
@@ -0,0 +1,23 @@
+import { COLLECTIONS } from '@/constants/collection'
+import { EVENT_BANNERS } from '@/mock/banner'
+import { store } from '@remote/firebase'
+import Button from '@shared/Button'
+import { collection, doc, writeBatch } from 'firebase/firestore'
+
+export default function EventBannerAddButton() {
+  const handleButtonClick = async () => {
+    const batch = writeBatch(store)
+
+    EVENT_BANNERS.forEach((banner) => {
+      const bannerRef = doc(collection(store, COLLECTIONS.EVENT_BANNER))
+
+      batch.set(bannerRef, banner)
+    })
+
+    await batch.commit()
+
+    alert('배너 데이터가 추가되었습니다')
+  }
+
+  return <Button onClick={handleButtonClick}>이벤트 배너 데이터 추가</Button>
+}
