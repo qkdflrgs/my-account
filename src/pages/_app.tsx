5d68b8e738502e12a05e3ece79301a27b3208fef
qkdflrgs <qkdflrgs@gmail.com>
2024-07-17 19:21:47 +0900

    feat(banner): add QueryClientProvider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import globalStyles from '@styles/globalStyles'
 import { Global } from '@emotion/react'
 import type { AppProps } from 'next/app'
+import Layout from '@shared/Layout'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const client = new QueryClient({})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Global styles={globalStyles} />
-      <Component {...pageProps} />
-    </>
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </Layout>
   )
 }
