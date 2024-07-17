531c2c5997d26da5259f912cd4598a6041ac7139
qkdflrgs <qkdflrgs@gmail.com>
2024-07-17 17:46:08 +0900

    feat(layout): add Layout component for SEO

---
+++ src/components/shared/Layout.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import SEO from './SEO'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div>
+      <SEO
+        title="My Account"
+        description="내 자산 관리를 보다 쉽게!"
+        image=""
+      />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      {children}
+    </div>
+  )
+}
