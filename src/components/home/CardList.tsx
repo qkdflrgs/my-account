ffeeec3e7aa2dfd6c462ced917bdb19fb045f0c0
qkdflrgs <qkdflrgs@gmail.com>
2024-07-19 02:40:25 +0900

    feat(recommend): add CardList component

---
+++ src/components/home/CardList.tsx
@@ -0,0 +1,79 @@
+import useCards from '@hooks/useCards'
+import withSuspense from '@shared/hocs/withSuspense'
+import Text from '@shared/Text'
+import ListRow from '@shared/ListRow'
+import Badge from '@shared/Badge'
+import Button from '@shared/Button'
+import Skeleton from '@shared/Skeleton'
+import { useRouter } from 'next/router'
+
+function CardList() {
+  const { data } = useCards()
+  const navigate = useRouter()
+
+  const isShowMoreButton = data?.items.length ?? 0 > 5
+
+  return (
+    <div style={{ padding: '24px 0' }}>
+      <Text
+        bold={true}
+        style={{ padding: '12px 24px', display: 'inline-block' }}
+      >
+        추천 카드
+      </Text>
+      <ul>
+        {data?.items
+          .slice(0, 5)
+          .map((card, index) => (
+            <ListRow
+              key={card.id}
+              contents={
+                <ListRow.Texts title={`${index + 1}위`} subTitle={card.name} />
+              }
+              right={card.payback && <Badge label={card.payback} />}
+              withArrow={true}
+              onClick={() => navigate.push(`/card/${card.id}`)}
+            />
+          ))}
+      </ul>
+      {isShowMoreButton && (
+        <div style={{ padding: '12px 24px 0px 24px' }}>
+          <Button
+            full={true}
+            weak={true}
+            size="medium"
+            onClick={() => navigate.push('/card')}
+          >
+            더보기
+          </Button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export function CardListSkeleton() {
+  return (
+    <div style={{ padding: '24px 0' }}>
+      <Text
+        bold={true}
+        style={{ padding: '12px 24px', display: 'inline-block' }}
+      >
+        추천 카드
+      </Text>
+      {[...new Array(5)].map((_, index) => (
+        <ListRow
+          key={index}
+          contents={
+            <ListRow.Texts
+              title={<Skeleton width={30} height={25} />}
+              subTitle={<Skeleton width={45} height={20} />}
+            />
+          }
+        />
+      ))}
+    </div>
+  )
+}
+
+export default withSuspense(CardList, { fallback: <CardListSkeleton /> })
