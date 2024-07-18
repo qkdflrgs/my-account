0308b07e2b886d086a6b0bbe2c64d06c5c558106
qkdflrgs <qkdflrgs@gmail.com>
2024-07-19 02:45:30 +0900

    feat(recommend): add Hydrate to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,21 @@ import globalStyles from '@styles/globalStyles'
 import { Global } from '@emotion/react'
 import type { AppProps } from 'next/app'
 import Layout from '@shared/Layout'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 
 const client = new QueryClient({})
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { dehydratedState, ...pageProps },
+}: AppProps) {
   return (
     <Layout>
       <Global styles={globalStyles} />
       <QueryClientProvider client={client}>
-        <Component {...pageProps} />
+        <Hydrate state={dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
       </QueryClientProvider>
     </Layout>
   )
