b18773f63fd9b844b1873ba758c0cc84f3f13a70
qkdflrgs <qkdflrgs@gmail.com>
2024-07-19 02:46:11 +0900

    feat(recommend): add CardList to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,21 @@ import Account from '@components/home/Account'
 import { CreditScoreSkeleton } from '@components/home/CreditScore'
 import { BannerSkeleton } from '@components/home/EventBanners'
 import dynamic from 'next/dynamic'
+import { CardListSkeleton } from '@components/home/CardList'
 
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
-  loading: () => <BannerSkeleton />,
   ssr: false,
+  loading: () => <BannerSkeleton />,
 })
 
 const CreditScore = dynamic(() => import('@components/home/CreditScore'), {
-  loading: () => <CreditScoreSkeleton />,
   ssr: false,
+  loading: () => <CreditScoreSkeleton />,
+})
+
+const CardList = dynamic(() => import('@components/home/CardList'), {
+  ssr: false,
+  loading: () => <CardListSkeleton />,
 })
 
 export default function Home() {
@@ -21,6 +27,8 @@ export default function Home() {
       <Account />
       <Spacing size={8} backgroundColor="grey100" />
       <CreditScore />
+      <Spacing size={8} backgroundColor="grey100" />
+      <CardList />
     </>
   )
 }
