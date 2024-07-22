f34dbb01602b75d7a06b18be65c89715d51a49ca
qkdflrgs <qkdflrgs@gmail.com>
2024-07-23 04:18:12 +0900

    feat(newAccount): add getAccount for fetching Account data

--- src/remote/account.ts
+++ src/remote/account.ts
@@ -1,6 +1,7 @@
 import { collection, doc, getDoc, setDoc } from 'firebase/firestore'
 import { store } from './firebase'
 import { COLLECTIONS } from '@constants/collection'
+import { Account } from '@models/account'
 
 export function setTerms({
   userId,
@@ -27,5 +28,27 @@ export async function getTerms(userId: string) {
   return {
     id: snapshot.id,
     ...(snapshot.data() as { userId: string; termIds: string[] }),
+  }
+}
+
+export function createAccount(newAccount: Account) {
+  return setDoc(
+    doc(collection(store, COLLECTIONS.ACCOUNT), newAccount.userId),
+    newAccount,
+  )
+}
+
+export async function getAccount(userId: string) {
+  const snapshot = await getDoc(
+    doc(collection(store, COLLECTIONS.ACCOUNT), userId),
+  )
+
+  if (snapshot.exists() === false) {
+    return null
+  }
+
+  return {
+    id: snapshot.id,
+    ...(snapshot.data() as Account),
   }
 }
