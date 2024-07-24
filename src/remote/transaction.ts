a648c43e7d263a9f52fffea1b73663d9e866d7f0
qkdflrgs <qkdflrgs@gmail.com>
2024-07-24 23:46:53 +0900

    feat(detail): add createTransaction for creating new transaction

---
+++ src/remote/transaction.ts
@@ -0,0 +1,8 @@
+import { Transaction } from '@models/transaction'
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { store } from './firebase'
+import { COLLECTIONS } from '@/constants/collection'
+
+export function createTransaction(newTransaction: Transaction) {
+  return setDoc(doc(collection(store, COLLECTIONS.TRANSACTION)), newTransaction)
+}
