6929f27c986900133c2b3248090d7a7e110e1d6d
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-07-26 02:30:18 +0900

    feat(myTerms): add Terms page to My page (#17)
    * feat(myTerms): add MyTerms button to My page
    
    * feat(myTerms): add Terms page
    
    * feat(myTerms): add mutation for updating user terms data
    
    * feat(myTerms): add updateTerms function for updating user terms data

---
+++ src/pages/settings/terms.tsx
@@ -0,0 +1,129 @@
+import useUser from '@hooks/useUser'
+import { getTerms, updateTerms } from '@remote/account'
+import { User } from '@models/user'
+import { GetServerSidePropsContext } from 'next'
+import { getSession } from 'next-auth/react'
+import {
+  dehydrate,
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from 'react-query'
+import { useMemo } from 'react'
+import { TERM_LIST } from '@constants/account'
+import Top from '@shared/Top'
+import Text from '@shared/Text'
+import ListRow from '@shared/ListRow'
+import Button from '@shared/Button'
+
+export default function TermsPage() {
+  const user = useUser()
+  const client = useQueryClient()
+  const { data } = useQuery({
+    queryKey: ['terms', user?.id],
+    queryFn: () => getTerms(user?.id as string),
+    enabled: user != null,
+  })
+
+  const { mutate, isLoading } = useMutation(
+    (termIds: string[]) => updateTerms(user?.id as string, termIds),
+    {
+      onSuccess: () => {
+        client.invalidateQueries(['terms', user?.id])
+      },
+    },
+  )
+
+  const agreedTerms = useMemo(() => {
+    if (data == null) {
+      return null
+    }
+
+    // 유저가 동의한 약관 목록
+    const allAgreedTerms = TERM_LIST.filter(({ id }) =>
+      data.termIds.includes(id),
+    )
+
+    // 필수 약관
+    const mandatoryTerms = allAgreedTerms.filter(
+      ({ mandatory }) => mandatory === true,
+    )
+
+    // 선택 약관
+    const nonMandatoryTerms = allAgreedTerms.filter(
+      ({ mandatory }) => mandatory === false,
+    )
+
+    return { mandatoryTerms, nonMandatoryTerms }
+  }, [data])
+
+  const handleDisagree = (selectedTermId: string) => {
+    const updatedTermIds = data?.termIds.filter(
+      (termId) => selectedTermId !== termId,
+    )
+
+    if (updatedTermIds != null) {
+      mutate(updatedTermIds)
+    }
+  }
+
+  return (
+    <div>
+      <Top title="약관" subTitle="약관 리스트 및 철회" />
+      {agreedTerms == null ? (
+        <Text>동의한 약관 목록이 없습니다.</Text>
+      ) : (
+        <ul>
+          {agreedTerms.mandatoryTerms.map((term) => (
+            <ListRow
+              key={term.id}
+              contents={
+                <ListRow.Texts title={`[필수] ${term.title}`} subTitle="" />
+              }
+            />
+          ))}
+          {agreedTerms.nonMandatoryTerms.map((term) => (
+            <ListRow
+              key={term.id}
+              contents={
+                <ListRow.Texts title={`[선택] ${term.title}`} subTitle="" />
+              }
+              right={
+                <Button
+                  disabled={isLoading === true}
+                  onClick={() => handleDisagree(term.id)}
+                >
+                  철회
+                </Button>
+              }
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+
+  if (session != null && session.user != null) {
+    const client = new QueryClient()
+
+    await client.prefetchQuery({
+      queryKey: ['terms', (session.user as User).id],
+      queryFn: () => getTerms((session.user as User).id),
+    })
+
+    return {
+      props: {
+        dehydratedState: JSON.parse(JSON.stringify(dehydrate(client))),
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
