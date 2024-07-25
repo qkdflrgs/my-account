b362f6d1bd175f0aa11d99509783563809d74f4b
qkdflrgs <qkdflrgs@gmail.com>
2024-07-25 15:54:53 +0900

    feat(detail): refactoring useTransactions hook(adding filter prop)

--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -1,15 +1,17 @@
 import { useInfiniteQuery } from 'react-query'
 import useUser from './useUser'
-import { getTransactions } from '@/remote/transaction'
+import { getTransactions } from '@remote/transaction'
+import { TransactionFilterType } from '@models/transaction'
 
 export default function useTransactions({
   suspense,
-}: { suspense?: boolean } = {}) {
+  filter,
+}: { suspense?: boolean; filter?: TransactionFilterType } = {}) {
   const user = useUser()
   return useInfiniteQuery({
-    queryKey: ['transactions', user?.id],
+    queryKey: ['transactions', user?.id, filter],
     queryFn: ({ pageParam }) =>
-      getTransactions({ pageParam, userId: user?.id as string }),
+      getTransactions({ pageParam, userId: user?.id as string, filter }),
     getNextPageParam: (snapshot) => {
       return snapshot.lastVisible
     },
