bcc56233d1c2d132347ff52449d3eed89e6a5015
qkdflrgs <qkdflrgs@gmail.com>
2024-07-26 00:33:59 +0900

    feat(piggyBank): refactoring PiggyBankRow with useQuery for fetching piggybank data

--- src/components/account/PiggyBankRow.tsx
+++ src/components/account/PiggyBankRow.tsx
@@ -1,9 +1,53 @@
 import Image from 'next/image'
-import ListRow from '../shared/ListRow'
+import ListRow from '@shared/ListRow'
 import { useRouter } from 'next/router'
+import withSuspense from '@shared/hocs/withSuspense'
+import useUser from '@hooks/useUser'
+import { useQuery } from 'react-query'
+import { getPiggybank } from '@/remote/piggybank'
+import { differenceInDays } from 'date-fns'
+import Flex from '@shared/Flex'
+import Text from '@shared/Text'
+import addDelimiter from '@/utils/addDelimiter'
 
-export default function PiggyBankRow() {
+function PiggyBankRow() {
   const navigate = useRouter()
+  const user = useUser()
+
+  const { data } = useQuery({
+    queryKey: ['piggybank', user?.id],
+    queryFn: () => getPiggybank(user?.id as string),
+    suspense: true,
+  })
+
+  if (data == null) {
+    return (
+      <div>
+        <ul>
+          <ListRow
+            left={
+              <Image
+                src="https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-24-256.png"
+                width={40}
+                height={40}
+                alt="piggy bank"
+              />
+            }
+            contents={
+              <ListRow.Texts
+                title="저금통"
+                subTitle="매일 매일 조금씩 저금하여 목표 금액을 모아보세요"
+              />
+            }
+            withArrow={true}
+            onClick={() => navigate.push('/account/piggybank/new')}
+          />
+        </ul>
+      </div>
+    )
+  }
+
+  const dday = differenceInDays(data.endDate, new Date())
 
   return (
     <div>
@@ -18,10 +62,14 @@ export default function PiggyBankRow() {
             />
           }
           contents={
-            <ListRow.Texts
-              title="저금통"
-              subTitle="매일 매일 조금씩 저금하여 목표 금액을 모아보세요"
-            />
+            <Flex direction="column">
+              <Text typography="t4" bold={true}>
+                D-{dday}
+              </Text>
+              <Text>
+                {addDelimiter(data.goalAmount - data.balance)}원 남았어요
+              </Text>
+            </Flex>
           }
           withArrow={true}
           onClick={() => navigate.push('/account/piggybank/new')}
@@ -30,3 +78,7 @@ export default function PiggyBankRow() {
     </div>
   )
 }
+
+export default withSuspense(PiggyBankRow, {
+  fallback: <div>로딩중...</div>,
+})
